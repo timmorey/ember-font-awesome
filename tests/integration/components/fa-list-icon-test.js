8f8e581b3496e771091bd6225388c498abfdc74c
Marten Schilstra <mail@martenschilstra.nl>
2015-10-19 00:05:23 +0200

    Add {{fa-list}} and {{fa-list-icon}} components

---
+++ tests/integration/components/fa-list-icon-test.js
@@ -0,0 +1,14 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('fa-list-icon', 'Integration | Component | {{fa-list-icon}}', {
+  integration: true
+});
+
+test('it is an icon with the fa-li class on it', function(assert) {
+  this.render(hbs`{{fa-list-icon}}`);
+
+  const $icon = this.$('i');
+  assert.equal($icon.length, 1);
+  assert.ok($icon.hasClass('fa-li'));
+});
---
+++ tests/integration/components/fa-list-test.js
@@ -0,0 +1,26 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('fa-list', 'Integration | Component | {{fa-list}}', {
+  integration: true
+});
+
+test('it is an ul tag with fa-ul class', function(assert) {
+  this.render(hbs`{{fa-list}}`);
+
+  const $list = this.$('ul');
+  assert.equal($list.length, 1);
+  assert.ok($list.hasClass('fa-ul'));
+});
+
+test('it yields to a block', function(assert) {
+  this.render(hbs`
+    {{#fa-list}}
+      <li>{{fa-list-icon icon="star"}}Item 1</li>
+      <li>{{fa-list-icon icon="star"}}Item 2</li>
+    {{/fa-list}}
+  `);
+
+  const $icons = this.$('i');
+  assert.equal($icons.length, 2);
+});
