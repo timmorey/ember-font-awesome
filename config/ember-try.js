b227d8308bc59b5736115f02acfc4ceb2e3e789d
Ryan Panning <panman82@gmail.com>
2016-03-03 10:47:08 -0600

    Update to ember-cli 2.4.1
    Updated ember-cli to version 2.4.1 and also ran `ember init` (only
    applying changes where they made sense). Finally added an ember-try
    scenario for ember 2.4.1.

--- config/ember-try.js
+++ config/ember-try.js
@@ -29,6 +29,12 @@ module.exports = {
       dependencies: {
         'ember': '2.3.1'
       }
+    },
+    {
+      name: 'ember-2.4.X',
+      dependencies: {
+        'ember': '2.4.1'
+      }
     }
   ]
 };
