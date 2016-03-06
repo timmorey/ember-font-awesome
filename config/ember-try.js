c75a08f96a649891b94cb4635d7dc1830e7b1ad0
Marten Schilstra <mail@martenschilstra.nl>
2016-03-06 10:17:35 +0100

    Stop testing pre 2.3 Ember versions

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,30 +1,6 @@
 module.exports = {
   scenarios: [
     {
-      name: 'ember-1.13.X',
-      dependencies: {
-        'ember': '1.13.13'
-      }
-    },
-    {
-      name: 'ember-2.0.X',
-      dependencies: {
-        'ember': '2.0.3'
-      }
-    },
-    {
-      name: 'ember-2.1.X',
-      dependencies: {
-        'ember': '2.1.2'
-      }
-    },
-    {
-      name: 'ember-2.2.X',
-      dependencies: {
-        'ember': '2.2.2'
-      }
-    },
-    {
       name: 'ember-2.3.X',
       dependencies: {
         'ember': '2.3.1'
