cfda1a40c7c0bd61b22033a0938a022646437a51
Chris Thoburn <runspired@gmail.com>
2016-05-20 22:31:17 -0500

    fix(index.js): enables usage by in-repo-addons and in-repo-engines

--- index.js
+++ index.js
@@ -21,11 +21,25 @@ module.exports = {
   },
 
   included: function(app, parentAddon) {
+
     // Quick fix for add-on nesting
-    // https://github.com/ember-cli/ember-cli/issues/3718
     // https://github.com/aexmachina/ember-cli-sass/blob/v5.3.0/index.js#L73-L75
-    if (typeof app.import !== 'function' && app.app) {
-      this.app = app = app.app;
+    // see: https://github.com/ember-cli/ember-cli/issues/3718
+    while (typeof app.import !== 'function' && (app.app || app.parent)) {
+      app = app.app || app.parent;
+    }
+
+    // if app.import and parentAddon are blank, we're probably being consumed by an in-repo-addon
+    // or engine, for which the "bust through" technique above does not work.
+    if (typeof app.import !== 'function' && !parentAddon) {
+      if (app.registry && app.registry.app) {
+        app = app.registry.app;
+      }
+    }
+
+    if (!parentAddon && typeof app.import !== 'function') {
+      throw new Error('ember-font-awesome is being used within another addon or engine and is' +
+        ' having trouble registering itself to the parent application.');
     }
 
     // https://github.com/ember-cli/ember-cli/issues/3718#issuecomment-88122543
